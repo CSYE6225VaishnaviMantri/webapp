0b1635bb122d771e65b4f7487f46e7853d589aa5
VaishnaviNEU <mantri.v@northeastern.edu>
2024-02-21 15:40:11 -0500

    PO

--- MySqlInstallation.sh
+++ MySqlInstallation.sh
@@ -8,6 +8,7 @@ echo "Installing MySQL"
 sudo dnf install mysql-server -y
 sudo systemctl start mysqld.service
 mysql -u root  -e  "ALTER USER 'root'@'localhost' IDENTIFIED BY 'root';"
+
 # Start and enable MySQL service
 sudo systemctl start mysqld
 sudo systemctl enable mysqld
