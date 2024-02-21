d0d30280586d38df01cd028f6cd64989032da028
VaishnaviNEU <mantri.v@northeastern.edu>
2024-02-21 15:43:49 -0500

    PSUH

--- MySqlInstallation.sh
+++ MySqlInstallation.sh
@@ -7,10 +7,7 @@ sudo dnf install java-17-openjdk -y
 echo "Installing MySQL"
 sudo dnf install mysql-server -y
 sudo systemctl start mysqld.service
-mysql -u root  -e "CREATE DATABASE webappDB;"
-mysql -u root  -e "CREATE USER 'clouduser'@'localhost' IDENTIFIED BY 'clouduser';"
-mysql -u root  -e "GRANT ALL ON . TO 'clouduser'@'localhost';"
-mysql -u root  -e "FLUSH PRIVILEGES;"
+mysql -u root  -e  "ALTER USER 'root'@'localhost' IDENTIFIED BY 'root';"
 
 # Start and enable MySQL service
 sudo systemctl start mysqld
