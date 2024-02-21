2ad28c18d3e080d3b0e50bd56400fcdbd2c7d5fb
VaishnaviNEU <mantri.v@northeastern.edu>
2024-02-20 22:06:09 -0500

    Pushing

--- JavaInstallation.sh
+++ JavaInstallation.sh
@@ -5,24 +5,16 @@ sudo dnf upgrade -y
 
 #!/bin/bash
 
-# Install required tools
+# Install Java 17
+echo "Installing Java"
 sudo yum -y install wget vim
-
-# Download and install Oracle JDK 17
 wget https://download.oracle.com/java/17/latest/jdk-17_linux-x64_bin.rpm
-sudo yum -y localinstall ./jdk-17_linux-x64_bin.rpm
+sudo yum -y install ./jdk-17_linux-x64_bin.rpm
 
-# Update JDK version and set environment variable
+# Update jdk version
 echo "Updating JDK Version"
-export JAVA_HOME=/usr/lib/jvm/jdk-17-oracle-x64
-echo "export JAVA_HOME=$JAVA_HOME" >> ~/.bashrc
-echo "export PATH=\$PATH:\$JAVA_HOME/bin" >> ~/.bashrc
+export JAVA_HOME=/usr/lib/jvm/jdk-17-oracle-x64/>> ~/.source /etc/environment
 
-# Make changes to the current session
-source ~/.bashrc
-
-# Verify installation
-java --version
 
 # Clean up downloaded RPM file
 echo "JDK 17 Installation Completed."
