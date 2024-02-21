5b044f167c616858a0f1254f6670f210ffa264f4
VaishnaviNEU <mantri.v@northeastern.edu>
2024-02-21 01:48:04 -0500

    Updating and Testing

--- MySqlInstallation.sh
+++ MySqlInstallation.sh
@@ -6,31 +6,17 @@ sudo dnf upgrade -y
 echo "Starting with MySQL Installation."
 sudo dnf install mysql-server -y
  
+ 
+sudo systemctl start mysqld.service
+sudo systemctl enable mysqld.service
+
+mysql -u root  -e "CREATE USER 'clouduser'@'localhost' IDENTIFIED BY 'clouduser';"
+mysql -u root  -e "GRANT ALL ON *.* TO 'clouduser'@'localhost';"
+mysql -u root  -e "FLUSH PRIVILEGES;"
+ 
+# Start and enable MySQL service
 sudo systemctl start mysqld
 sudo systemctl enable mysqld
- 
-SECURE_INSTALLATION=$(expect -c "
-spawn sudo mysql_secure_installation
-expect \"Enter current password for root (enter for none):\"
-send \"\r\"
-expect \"Set root password?\"
-send \"y\r\"
-expect \"New password:\"
-send \"data123\r\"
-expect \"Re-enter new password:\"
-send \"data123\r\"
-expect \"Remove anonymous users?\"
-send \"y\r\"
-expect \"Disallow root login remotely?\"
-send \"y\r\"
-expect \"Remove test database and access to it?\"
-send \"y\r\"
-expect \"Reload privilege tables now?\"
-send \"y\r\"
-expect eof
-")
- 
-echo "$SECURE_INSTALLATION"
  
 echo "Completed with MySQL Installation."
  
