cdf0cb2ab83d9134dea7df52bac461f786e375f0
VaishnaviNEU <mantri.v@northeastern.edu>
2024-02-21 14:50:52 -0500

    SQL Testing

--- MySqlInstallation.sh
+++ MySqlInstallation.sh
@@ -1,18 +1,17 @@
 #!/bin/bash
-
+ 
 sudo dnf upgrade -y
 sudo dnf install java-17-openjdk -y
-
+ 
 # Install MySQL
 echo "Installing MySQL"
 sudo dnf install mysql-server -y
 sudo systemctl start mysqld.service
-mysql -u root  -e "CREATE DATABASE webappDB;"
+mysql -u root  -e "CREATE DATABASE UserDatabase;"
 mysql -u root  -e "CREATE USER 'clouduser'@'localhost' IDENTIFIED BY 'clouduser';"
 mysql -u root  -e "GRANT ALL ON . TO 'clouduser'@'localhost';"
 mysql -u root  -e "FLUSH PRIVILEGES;"
-
+ 
 # Start and enable MySQL service
 sudo systemctl start mysqld
 sudo systemctl enable mysqld
- 
