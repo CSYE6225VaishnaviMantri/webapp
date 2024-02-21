68efcdb54f88a4ef87fe43fdf0d858dfe235dbb6
VaishnaviNEU <mantri.v@northeastern.edu>
2024-02-21 17:19:06 -0500

    Pushing For Failure Tests

--- MySqlInstallation.sh
+++ MySqlInstallation.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
  
 sudo dnf upgrade -y
+
+echo "Starting with Java Installation."
 sudo dnf install java-17-openjdk -y
+echo "Completed with Java Installation."
  
 # Install MySQL
 echo "Installing MySQL"
+
 sudo dnf install mysql-server -y
 sudo systemctl start mysqld.service
 mysql -u root  -e  "ALTER USER 'root'@'localhost' IDENTIFIED BY 'root';"
@@ -12,3 +16,6 @@ mysql -u root  -e  "ALTER USER 'root'@'localhost' IDENTIFIED BY 'root';"
 # Start and enable MySQL service
 sudo systemctl start mysqld
 sudo systemctl enable mysqld
+
+echo "Completed Installing MySQL"
+
