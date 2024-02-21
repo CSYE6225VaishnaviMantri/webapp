593ae5b5f749aea4af150c2721d71d0bdfd79a53
VaishnaviNEU <mantri.v@northeastern.edu>
2024-02-21 13:35:46 -0500

    Pushing SQL

--- MySqlInstallation.sh
+++ MySqlInstallation.sh
@@ -9,7 +9,7 @@ echo "Completed with Java Installation."
 echo "Starting with MySQL Installation."
 sudo dnf install mysql-server -y
 sudo systemctl start mysqld.service
-mysql -u root  -e "CREATE USER 'clouduser'@'localhost' IDENTIFIED BY 'clouduser';"
+mysql -u root  -e "CREATE USER 'clouduser'@'localhost' IDENTIFIED BY 'data123';"
 mysql -u root  -e "GRANT ALL ON *.* TO 'clouduser'@'localhost';"
 mysql -u root  -e "FLUSH PRIVILEGES;"
 sudo systemctl start mysqld
