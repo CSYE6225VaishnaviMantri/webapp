75ec90afe2a888e2843d70c773622e2d4b72107c
VaishnaviNEU <mantri.v@northeastern.edu>
2024-02-21 03:20:17 -0500

    Pushing Java

--- JavaInstallation.sh
+++ JavaInstallation.sh
@@ -13,13 +13,13 @@ sudo dnf install java-17-openjdk -y
 java -version
 echo "JDK 17 Installation Completed."
 
-echo "Starting with Maven Installation."
-sudo dnf install maven -y
-echo "Completed Maven Installation."
+# echo "Starting with Maven Installation."
+# sudo dnf install maven -y
+# echo "Completed Maven Installation."
 
-echo "Starting with Tomcat Installation."
-sudo dnf install -y tomcat
+# echo "Starting with Tomcat Installation."
+# sudo dnf install -y tomcat
  
-sudo systemctl start tomcat
-sudo systemctl enable tomcat
-echo "Completed Installing Tomcat."
+# sudo systemctl start tomcat
+# sudo systemctl enable tomcat
+# echo "Completed Installing Tomcat."
