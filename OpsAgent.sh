d57d48716dbfa4044a4d48a4268e082840759725
VaishnaviNEU <mantri.v@northeastern.edu>
2024-03-19 10:08:28 -0400

    VARLOGS

--- OpsAgent.sh
+++ OpsAgent.sh
@@ -6,7 +6,7 @@ logging:
     my-app-receiver:
       type: files
       include_paths:
-        - /tmp/logs/application.log
+        - /tmp/var/logs/application.log
       record_log_file_path: true
   processors:
     my-app-processor:
