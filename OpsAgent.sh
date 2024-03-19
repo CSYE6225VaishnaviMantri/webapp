4bebef5dc0578b7f8fee96596d6bf0c76f4ce82e
VaishnaviNEU <mantri.v@northeastern.edu>
2024-03-18 21:22:53 -0400

    ops Agent

--- OpsAgent.sh
+++ OpsAgent.sh
@@ -1,6 +1,5 @@
 sudo chown -R csye6225:csye6225 /etc/google-cloud-ops-agent/
 
-
 cat <<EOF | sudo tee /etc/google-cloud-ops-agent/config.yaml
 logging:
   receivers:
@@ -10,33 +9,15 @@ logging:
         - /tmp/logs/application.log
       record_log_file_path: true
   processors:
-    add_severity_labels:
-      type: add_value
-      target_key: severity
-      value: "$${ctx:severity}"
-    add_labels:
-      type: add_value
-      target_key: labels
-      value: "$${ctx:labels}"
-    add_http_request_method:
-      type: add_value
-      target_key: httpRequestMethod
-      value: "$${ctx:httpMethod}"
-    add_path:
-      type: add_value
-      target_key: path
-      value: "${ctx:path}"
-    add_timestamp:
-      type: add_value
-      target_key: timestamp
-      value: "$${date:yyyy-MM-dd HH:mm:ss.SSS}"
+    my-app-processor:
+      type: parse_json
+      time_key: time
+      time_format: "%Y-%m-%dT%H:%M:%S.%L%Z"
   service:
     pipelines:
       default_pipeline:
         receivers: [my-app-receiver]
-        processors: [add_severity_labels, add_labels, add_http_request_method, add_path, add_timestamp]
-        
+        processors: [my-app-processor]
 EOF
-
 
 sudo systemctl restart google-cloud-ops-agent
