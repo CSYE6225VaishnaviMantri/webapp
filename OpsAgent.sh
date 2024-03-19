14d4e0d0ec06f911017564ed8d5bdde878ed2474
VaishnaviNEU <145122714+VaishnaviNEU@users.noreply.github.com>
2024-03-19 19:23:09 -0400

    Update OpsAgent.sh

--- OpsAgent.sh
+++ OpsAgent.sh
@@ -6,7 +6,7 @@ logging:
     my-app-receiver:
       type: files
       include_paths:
-        - /var/logs/webapp/application.log
+        - /tmp/var/logs/webapp/application.log
       record_log_file_path: true
   processors:
     my-app-processor:
