962f8a4020dbef05b8215cfcd2297d87c38bf6db
VaishnaviNEU <mantri.v@northeastern.edu>
2024-03-20 14:01:37 -0400

    message

--- src/main/java/com/Web/Application/Cloud/Web/App/controller/UserController.java
+++ src/main/java/com/Web/Application/Cloud/Web/App/controller/UserController.java
@@ -506,7 +506,7 @@ public class UserController {
 
         ThreadContext.put("severity", "DEBUG");
 
-        log.debug("Validating password format...");
+        log.debug("Validating Email format...");
 
         String regularExpression = "^[a-zA-Z0-9_+&*-]+(?:\\.[a-zA-Z0-9_+&*-]+)*@(?:[a-zA-Z0-9-]+\\.)+[a-zA-Z]{2,}$";
 
@@ -535,9 +535,6 @@ public class UserController {
     }
 
 
+
 }
-
-
-
-
 
